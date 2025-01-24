e01f30a0122e69cd099586814ad5a1cda2f74ee8
Aizhan Tuleyeva <tuleevaajzan5@gmail.com>
2025-01-24 22:04:54 +0300

    lab2

---
+++ lab2/Lists/Lists.py
@@ -0,0 +1,30 @@
+mylist = ["apple", "banana", "cherry"]
+
+#Lists are created using square brackets:
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#Lists allow duplicate values:
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+#Print the number of items in the list:
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#List items can be of any data type:
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+#A list can contain different data types:
+list1 = ["abc", 34, True, 40, "male"]
+
+#What is the data type of a list?
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+#Using the list() constructor to make a List:
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
