286ca1ccdc52d9d64c2a5e1ac699f2ab46aff29d
Aizhan Tuleyeva <tuleevaajzan5@gmail.com>
2025-02-03 02:20:50 +0300

    lab3

--- lab3/Classes/1.py
+++ lab3/Classes/1.py
@@ -2,24 +2,17 @@
 # `getString`: to get a string from console input
 # `printString`: to print the string in upper case.
 
-# class MyClass:
-#     def __init__(self):
-#         self.input_string = ""
+class MyClass:
+    def __init__(self):
+        self.input_string = ""
 
-#     def getString(self):
-#         # Get a string from console input
-#         self.input_string = input("Enter a string: ")
+    def getString(self):
+        self.input_string = input("Enter a string: ")
 
-#     def printString(self):
-#         # Print the string in upper case
-#         print(self.input_string.upper())
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    def printString(self):
+        print(self.input_string.upper())
 
-  def myfunc(self):
-    print("Hello my name is " + self.name)
 
-p1 = Person("John", 36)
-p1.myfunc()
+string = MyClass()
+string.getString() 
+string.printString() 
